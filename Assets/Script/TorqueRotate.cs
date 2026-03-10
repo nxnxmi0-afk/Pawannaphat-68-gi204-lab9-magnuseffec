ceabd0619f051486c97b3a7b9a140308c1ed3d8d
nxnxmi0-afk <nxnxmi0@gmail.com>
2026-03-10 10:55:40 +0700

    kick ball

--- Assets/Script/TorqueRotate.cs
+++ Assets/Script/TorqueRotate.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TorqueRotate : MonoBehaviour
 {
+    public float torqueRotate;
+
+    Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Keyboard.current.dKey.isPressed)
+        {
+            rb.AddTorque (new Vector3(0, torqueRotate, 0));
+        }
+        else
+        { rb.AddTorque( Vector3.zero); }
+
     }
 }
